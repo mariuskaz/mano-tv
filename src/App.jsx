8b4f180db3cba0056b91c436f363a88f70f4c491
Marius K. <marius.into@gmail.com>
2024-11-27 10:43:43 +0000

    get channels from programatv.lt

--- src/App.jsx
+++ src/App.jsx
@@ -4,50 +4,63 @@ import React, { useState, useEffect } from 'react';
 export default function App() {
   const [tvChannels, setTvChannels] = useState([]);
 
-  useEffect(() => {
-      const fetchTvProgram = async () => {
-          try {
-              const response = await fetch('https://api.codetabs.com/v1/proxy?quest=tv24.lt');
-              const text = await response.text();
-              
-              // Parse the HTML
-              const parser = new DOMParser();
-              const doc = parser.parseFromString(text, 'text/html');
+    useEffect(() => {
+        const fetchTvProgram = async () => {
+            try {
+                const response = await fetch('https://api.codetabs.com/v1/proxy?quest=https://www.programatv.lt/');
+                const text = await response.text();
+                
+                // Parse the HTML
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(text, 'text/html');
 
-              // Extract channel information
-              const channels = Array.from(doc.querySelectorAll('.schedule')).map(channel => {
-                  return {
-                      name: channel.querySelector('.front-channel-icon').textContent || "unknown",
-                      logo: channel.querySelector('img').src,
-                      title: channel.querySelector('.item-label a')?.textContent || "unknown",
-                      time: channel.querySelector('.item-time')?.textContent || "no time",
-                  };
-              });
+                // Extract channel information
+                const channels = Array.from(doc.querySelectorAll('.channel')).map(channel => {
 
-              // Update state with extracted channels
-              setTvChannels(channels);
+                    let pos = 0
+                    let schedule = Array.from(channel.querySelectorAll('.info, .info-down'))
 
-          } catch (error) {
-              console.error('Error fetching TV program:', error);
+                    schedule.map( (info, index) => {
+                        if (info.classList.contains('info-down')) pos = index
+                    })
 
-          }
-      };
+                    return {
+                        name: channel.querySelector('a').textContent || "unknown",
+                        logo: channel.querySelector('img').src.replace(location.origin,"https://www.programatv.lt"),
+                        time: schedule[pos].querySelector('.info-down strong')?.textContent,
+                        title: schedule[pos].querySelectorAll('.info-down strong')[1]?.textContent,
+                        nextTime: schedule[pos+1]?.querySelector('strong')?.textContent || "",
+                        nextTitle: schedule[pos+1]?.querySelector('span')?.textContent || "",
+                    };
+                });
 
-      fetchTvProgram();
-  }, []);
+                
 
-  return (
-      <div>
-          <h1>Test</h1>
-          <ul>
-              {tvChannels.map((channel, index) => (
-                  <li key={index}>
-                      <img src={channel.logo} alt={channel.name} />
-                      <span>{channel.name}</span>
-                      <p>{channel.time} {channel.title}</p>
-                  </li>
-              ))}
-          </ul>
-      </div>
-  );
+                // Update state with extracted channels
+                setTvChannels(channels);
+
+            } catch (error) {
+                console.error('Error fetching TV program:', error);
+
+            }
+        };
+
+        fetchTvProgram();
+    }, []);
+
+    return (
+        <div>
+            <h1>Test</h1>
+            <div>
+                {tvChannels.map((channel, index) => (
+                    <div key={index}>
+                        <img src={channel.logo} alt={channel.name} />
+                        <b>{channel.name}</b>
+                        <p>{channel.time} {channel.title}</p>
+                        <p>{channel.nextTime} {channel.nextTitle}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
