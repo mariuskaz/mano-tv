286cd6b0521997fd4dd399020f2d6f166e348852
Marius K. <marius.into@gmail.com>
2024-11-27 11:50:11 +0000

    add progress, update style

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 
 export default function App() {
-  const [tvChannels, setTvChannels] = useState([]);
+    const [channels, setChannels] = useState([]);
 
     useEffect(() => {
-        const fetchTvProgram = async () => {
+        const fetchProgram = async () => {
             try {
                 const response = await fetch('https://api.codetabs.com/v1/proxy?quest=https://www.programatv.lt/');
                 const text = await response.text();
@@ -15,7 +15,7 @@ export default function App() {
                 const doc = parser.parseFromString(text, 'text/html');
 
                 // Extract channel information
-                const channels = Array.from(doc.querySelectorAll('.channel')).map(channel => {
+                const program = Array.from(doc.querySelectorAll('.channel')).map(channel => {
 
                     let pos = 0
                     let schedule = Array.from(channel.querySelectorAll('.info, .info-down'))
@@ -24,20 +24,25 @@ export default function App() {
                         if (info.classList.contains('info-down')) pos = index
                     })
 
-                    return {
-                        name: channel.querySelector('a').textContent || "unknown",
-                        logo: channel.querySelector('img').src.replace(location.origin,"https://www.programatv.lt"),
-                        time: schedule[pos].querySelector('.info-down strong')?.textContent,
-                        title: schedule[pos].querySelectorAll('.info-down strong')[1]?.textContent,
-                        nextTime: schedule[pos+1]?.querySelector('strong')?.textContent || "",
-                        nextTitle: schedule[pos+1]?.querySelector('span')?.textContent || "",
-                    };
+                    const name = channel.querySelector('a').textContent
+                    const logo = channel.querySelector('img').src.replace(location.origin,"https://www.programatv.lt")
+                    const now = schedule[pos].querySelector('.info-down strong')?.textContent + " " +
+                                schedule[pos].querySelectorAll('.info-down strong')[1]?.textContent
+                    const next = (schedule[pos + 1]?.querySelector('strong')?.textContent || "") + " " +
+                                (schedule[pos + 1]?.querySelector('span')?.textContent || "")
+
+                    const url = "#"
+                    const start = parseDate(schedule[pos].querySelector('.info-down strong')?.textContent.trim())
+                    const stop = parseDate(schedule[pos + 1]?.querySelector('strong')?.textContent)
+
+                    const date = new Date()
+                    const progress = (date - start) * 100 / (stop - start)
+
+                    return { name, logo, url, progress, now, next }
                 });
 
-                
-
                 // Update state with extracted channels
-                setTvChannels(channels);
+                setChannels(program);
 
             } catch (error) {
                 console.error('Error fetching TV program:', error);
@@ -45,22 +50,46 @@ export default function App() {
             }
         };
 
-        fetchTvProgram();
+        fetchProgram();
     }, []);
 
-    return (
-        <div>
-            <h1>Test</h1>
-            <div>
-                {tvChannels.map((channel, index) => (
-                    <div key={index}>
-                        <img src={channel.logo} alt={channel.name} />
-                        <b>{channel.name}</b>
-                        <p>{channel.time} {channel.title}</p>
-                        <p>{channel.nextTime} {channel.nextTitle}</p>
-                    </div>
-                ))}
+    function parseDate(d = "00:00") {
+        let date = new Date(),
+        year = date.getFullYear(),
+        month = date.getMonth(),
+        day = date.getDate(),
+        h = d.substring(0, 2),
+        m = d.substring(3, 5)
+        return new Date(year, month, day, h, m)
+      }
+
+    function Label({ value }) {
+        return <span className='float-label'>{value}</span>
+    }
+
+    function Channels({ list }) {
+        return <div className='channels-list'>{list}</div>
+    }
+
+    function Channel({ channel }) {
+        return (
+          <div className='channel' onClick={() => window.open(channel.url)}>  
+            <div className='logo'><img src={channel.logo} alt="logo" />{channel.name}</div>
+            <div className='content'>
+              <div>{channel.now}</div><progress className='progress' value={channel.progress} max={100} /><div style={{color:'darkgrey'}}>{channel.next}</div>
             </div>
-        </div>
+          </div>
+        )
+      }
+
+    const myChannelsList = channels.map( (channel, index) => {
+        return <Channel channel={channel} key={index} />
+    })
+
+    return (
+        <>
+            <header>TV programa <Label value={channels.length}/></header>
+            <Channels list={myChannelsList}/>
+        </>
     );
 }
