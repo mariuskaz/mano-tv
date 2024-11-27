1e685bc6d3b528c052630e4a0b4ac2f62215b476
Marius K. <marius.into@gmail.com>
2024-11-27 13:59:53 +0000

    change epg source

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,197 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 
 export default function App() {
-    const [channels, setChannels] = useState([]);
-
+    const [guide, setGuide] = useState({})
+    const [synced, setSynced] = useState(false)
+    const [found, setFound] = useState(false)
+  
+    const epg_link = 'https://www.bevy.be/bevyfiles/lithuania3.xml'
+  
+    const channels = [
+      { 
+        id: "LRT Televizija.lt",
+        url: "https://www.lrt.lt/mediateka/tiesiogiai/lrt-televizija",
+        logo: "https://www.lrt.lt/images/logo/logo-lrt.svg?v=673",
+        name: "LRT",
+      },
+      { 
+        id: "LNK.lt",
+        url: "https://lnk.lt/video-embed/ad1a0439-7a2c-47d8-80c8-cf9b6b6a2f76/lnk-hd-kanalas-internetu?muteOnStart=true&autoStart=true&startFrom=undefined", //"https://lnk.lt/tiesiogiai",
+        logo: "https://content.tvprograma.lt/logo/220x80_0_88847800_1536058587m.png",
+        name: "LNK",
+      }, 
+      { 
+        id: "TV3.lt",
+        url: "https://play.tv3.lt/lives",
+        logo: "https://www.programatv.lt/images/channels/03.png?v=1", //"https://content.tvprograma.lt/logo/220x80_0_65492700_1578497500m.png",
+        name: "TV3",
+      },
+      { 
+        id: "BTV.lt",
+        url: "https://lnk.lt/tiesiogiai#btv",
+        logo: "https://content.tvprograma.lt/logo/220x80_0_99643600_1536832966m.png",
+        name: "BTV",
+      },
+      { 
+        id: "TV1.lt",
+        url: "https://lnk.lt/tiesiogiai#tv1",
+        logo: "https://content.tvprograma.lt/logo/220x80_0_50644700_1415689122m.png",
+        name: "TV1",
+      },
+      { 
+        id: "2TV.lt",
+        url: "https://lnk.lt/tiesiogiai#2tv",
+        logo: "https://content.tvprograma.lt/logo/220x80_0_88998600_1585550605m.png",
+        name: "2TV",
+      },
+      { 
+        id: "TV6.lt",
+        url: "https://play.tv3.lt/live/tv6-lt,live-2838694",
+        logo: "https://content.tvprograma.lt/logo/220x80_0_98820800_1665328884m.jpg",
+        name: "TV6",
+      },
+      { 
+        id: "Lietuvos ryto TV.lt",
+        url: "https://tv.lrytas.lt/live",
+        logo: "https://content.tvprograma.lt/logo/220x80_0_80098400_1538461908m.png",
+        name: "Lietuvos rytas",
+      },
+      { 
+        id: "LRT Plius.lt",
+        url: "https://www.lrt.lt/mediateka/tiesiogiai/lrt-plius",
+        logo: "https://content.tvprograma.lt/logo/220x80_0_91824600_1652106605m.jpg",
+        name: "LRT PLIUS",
+      },
+      { 
+        id: "Info TV.lt",
+        url: "https://lnk.lt/tiesiogiai#infotv",
+        logo: "https://content.tvprograma.lt/logo/220x80_0_34085600_1418495360m.png",
+        name: "Info TV",
+      },
+      { 
+        id: "Delfi TV.lt",
+        url: "https://www.delfi.lt/video/puslapis/tv",
+        logo: "https://content.tvprograma.lt/logo/220x80_0_11928400_1633001331m.png",
+        name: "Delfi",
+      },  
+      { 
+        id: "TV3 Plus.lt",
+        url: "https://play.tv3.lt/show/tv3-plus,live-4929289",
+        logo: "https://content.tvprograma.lt/logo/220x80_0_37190500_1654152447m.png",
+        name: "TV3 PLUS",
+      },
+    ]
+  
     useEffect(() => {
-        const fetchProgram = async () => {
-            try {
-                const response = await fetch('https://api.codetabs.com/v1/proxy?quest=https://www.programatv.lt/');
-                const text = await response.text();
-                
-                // Parse the HTML
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(text, 'text/html');
+      if (synced) return
+  
+      const epg_text = localStorage['epg']
 
-                // Extract channel information
-                const program = Array.from(doc.querySelectorAll('.channel')).map(channel => {
+      if (epg_text !== undefined) {
+        const parser = new DOMParser()
+        const xml = parser.parseFromString(epg_text, "application/xml")
+        const programs = xml.getElementsByTagName("programme")
+        const now = new Date()
 
-                    let pos = 0
-                    let schedule = Array.from(channel.querySelectorAll('.info, .info-down'))
+        let epg = {}
 
-                    schedule.map( (info, index) => {
-                        if (info.classList.contains('info-down')) pos = index
-                    })
-
-                    const name = channel.querySelector('a').textContent
-                    const logo = channel.querySelector('img').src.replace(location.origin,"https://www.programatv.lt")
-                    const now = schedule[pos].querySelector('.info-down strong')?.textContent + " " +
-                                schedule[pos].querySelectorAll('.info-down strong')[1]?.textContent
-                    const next = (schedule[pos + 1]?.querySelector('strong')?.textContent || "") + " " +
-                                (schedule[pos + 1]?.querySelector('span')?.textContent || "")
-
-                    const url = "#"
-                    const start = parseDate(schedule[pos].querySelector('.info-down strong')?.textContent.trim())
-                    const stop = parseDate(schedule[pos + 1]?.querySelector('strong')?.textContent)
-
-                    const date = new Date()
-                    const progress = (date - start) * 100 / (stop - start)
-
-                    return { name, logo, url, progress, now, next }
-                });
-
-                // Update state with extracted channels
-                setChannels(program);
-
-            } catch (error) {
-                console.error('Error fetching TV program:', error);
-
-            }
-        };
-
-        fetchProgram();
-    }, []);
-
-    function parseDate(d = "00:00") {
-        let date = new Date(),
-        year = date.getFullYear(),
-        month = date.getMonth(),
-        day = date.getDate(),
-        h = d.substring(0, 2),
-        m = d.substring(3, 5)
-        return new Date(year, month, day, h, m)
+        for (let item = 0; item < programs.length; item++) {
+          let channel = programs[item].getAttribute('channel') || ""
+          let title = programs[item].getElementsByTagName("title")[0]?.childNodes[0].nodeValue || ""
+  
+          let start = parseDate(programs[item].getAttribute('start'))
+          start.setTime(start.getTime() + 2 * 60 * 60 * 1000)
+  
+          let stop = parseDate(programs[item].getAttribute('stop'))
+          stop.setTime(stop.getTime() + 2 * 60 * 60 * 1000)
+  
+          if (epg[channel] === undefined) 
+            epg[channel] = { now: "", stop: "", next: "", progress: 0 }
+  
+          if (start <= now && stop >= now) {
+            epg[channel].now = start.toLocaleTimeString().substring(0, 5) + " " + title
+            epg[channel].progress = (now - start) * 100 / (stop - start)
+            epg[channel].stop = stop
+            if (!found) setFound(true)
+  
+          } else if (start <= epg[channel].stop) 
+            epg[channel].next = start.toLocaleTimeString().substring(0, 5) + " " + title
+  
+        }
+  
+        setGuide(epg)
+  
       }
-
-    function Label({ value }) {
-        return <span className='float-label'>{value}</span>
-    }
-
-    function Channels({ list }) {
-        return <div className='channels-list'>{list}</div>
-    }
-
-    function Channel({ channel }) {
-        return (
-          <div className='channel' onClick={() => window.open(channel.url)}>  
-            <div className='logo'><img src={channel.logo} alt="logo" />{channel.name}</div>
-            <div className='content'>
-              <div>{channel.now}</div><progress className='progress' value={channel.progress} max={100} /><div style={{color:'darkgrey'}}>{channel.next}</div>
-            </div>
-          </div>
-        )
-      }
-
-    const myChannelsList = channels.map( (channel, index) => {
-        return <Channel channel={channel} key={index} />
+  
+      setSynced(true)
+  
+    }, [synced, found])
+  
+    useLayoutEffect(() => {
+      document.addEventListener("visibilitychange", onVisibilityChange)
+      return () => document.removeEventListener("visibilitychange", onVisibilityChange)
     })
+  
+    function onVisibilityChange() {
+      if (document.visibilityState === 'visible') setSynced(false)
+    }
+  
+    function parseDate(d) {
+      const year = d.substring(0, 4),
+      month = d.substring(4, 6),
+      day = d.substring(6, 8),
+      h = d.substring(8, 10),
+      m = d.substring(10, 12)
+      return new Date(year, month - 1, day, h, m)
+    }
+  
+    function fetchGuide() {
+      fetch('https://api.codetabs.com/v1/proxy?quest=' + epg_link)
+      .then((res) => res.text())
 
+      .then(text => { 
+        localStorage.setItem("epg", text)
+        setSynced(false)
+      })
+
+      .catch(err => {
+        console.error(err.message)
+      })
+    }
+  
+    const Label = ({ value }) => {
+      return <span className='float-label'>{value}</span>
+    }
+  
+    const Toast = ({ message, button, action }) => {
+        return <div className='toast'>{message}<span className='toast-button' onClick={action}>{button}</span></div>
+    }
+  
+    const Channel = ({ channel }) => {
+      return (
+        <div className='channel' onClick={() => window.open(channel.url)}>  
+          <div className='logo'><img src={channel.logo} alt="logo" />{channel.name}</div>
+          <div className='content'>
+            <div>{guide[channel.id]?.now}</div><progress className='progress' value={guide[channel.id]?.progress} max={100} /><div style={{color:'darkgrey'}}>{guide[channel.id]?.next}</div>
+          </div>
+        </div>
+      )
+    }
+  
+    const Channels = ({ list }) => {
+      return <div className='channels-list'>{list}</div>
+    }
+  
+    const myChannelsList = channels.map( channel => {
+      return <Channel channel={channel} key={channel.id} />
+    })
+  
     return (
-        <>
-            <header>TV programa <Label value={channels.length}/></header>
-            <Channels list={myChannelsList}/>
-        </>
-    );
+      <>
+        <header>TV programa <Label value={channels.length} /></header>
+        <Channels list={myChannelsList} />
+        {!found && <Toast message={"EPG data not found!"} button={"Update"} action={fetchGuide} />}
+      </>
+    )
 }
